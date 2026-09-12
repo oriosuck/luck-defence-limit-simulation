e54139a0bb4bc5fdef187fcf5c5d2a8e41eb458f
oriosuck <jeongmnju@naver.com>
2026-09-12 15:57:21 +0900

    Add extensible character data

---
+++ src/data/characters.js
@@ -0,0 +1,9 @@
+// 캐릭터별 차이는 표시 정보만 관리한다.
+// 신규 캐릭터 추가 시 이 배열에 항목을 추가하고 image 경로만 연결하면 된다.
+export const CHARACTERS = [
+  { id: 'hero-1', name: '캐릭터 1', image: null },
+  { id: 'hero-2', name: '캐릭터 2', image: null },
+  { id: 'hero-3', name: '캐릭터 3', image: null },
+  { id: 'hero-4', name: '캐릭터 4', image: null },
+  { id: 'hero-5', name: '캐릭터 5', image: null },
+];
